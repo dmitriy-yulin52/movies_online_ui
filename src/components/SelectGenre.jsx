249c6c0e7932c0da99a0dc1d3eed5d76e4b05dfe
yulin dima <yulin_dimon@mail.ru>
2022-07-04 11:28:02 +0300

    add TVSHows component and fix Netflix component

--- src/components/SelectGenre.jsx
+++ src/components/SelectGenre.jsx
@@ -26,6 +26,11 @@ const SelectGenre = memo(({genres,type}) => {
 
 
 const Select = styled.select`
+margin-left:5rem;
+cursor:pointer;
+font-size:1.4rem;
+background-color:rgba(0,0,0,0.4);
+color:white;
 
 `
 
---
+++ src/pages/TVShows.jsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import {useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchMovies, getGenres} from "../store/netflix/actions";
+import {useCallback, useEffect, useState} from "react";
+import {onAuthStateChanged} from "firebase/auth";
+import {firebaseAuth} from "../utils/firebase-config";
+import styled from 'styled-components'
+import NavBar from "../components/NavBar";
+import Slider from "../components/Slider";
+import NotAvailable from "../components/NotAvailable";
+import SelectGenre from "../components/SelectGenre";
+import Spinner from "../components/spinner";
+
+const TVShows = () => {
+
+    const navigate = useNavigate()
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    const genresLoaded = useSelector((state) => state.netflix.genresLoaded)
+    const movies = useSelector((state) => state.netflix.movies)
+    const genres = useSelector((state) => state.netflix.genres)
+    const loadingMovies = useSelector((state) => state.netflix.loadingMovies)
+
+
+    const onClickNavigate = useCallback(() => {
+        navigate('/player')
+    }, [navigate])
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getGenres())
+    }, [])
+
+    useEffect(() => {
+        if (genresLoaded) dispatch(fetchMovies({type: 'tv'}))
+    }, [genresLoaded])
+
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset !== 0)
+        return () => window.onscroll = null
+    }
+
+    onAuthStateChanged(firebaseAuth, (currentUser) => {
+        // if (currentUser) navigate('/')
+    })
+
+
+    return (
+        <Container>
+            <div className="navbar">
+                <NavBar isScrolled={isScrolled}/>
+            </div>
+            <div className="data">
+                <SelectGenre genres={genres} type={'tv'}/>
+                {loadingMovies ? <Spinner/> : movies.length ? <Slider movies={movies}/> : <NotAvailable/>}
+            </div>
+        </Container>
+    );
+};
+
+
+const Container = styled.div`
+.data{
+    margin-top:8rem;
+    .not-available{
+        text-align:center;
+        color:white;
+        margin-top:4rem;
+    }
+}
+
+`
+
+
+export default TVShows;
